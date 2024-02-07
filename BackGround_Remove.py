95c3f1ab91834eb1bfa7047335669c2387ea998e
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-07 19:23:19 +0900

    faet : Background Remove Code fix & img add

--- BackGround_Remove.py
+++ BackGround_Remove.py
@@ -4,6 +4,6 @@ from PIL import Image
 
 
 # 이미지 배경 제거
-input = Image.open('/Users/seunghunjang/Desktop/Coordination/Coordination/train/BOTTOM/BOTTOM_5.jpg') # load image
+input = Image.open('path/to/remove/img.png') # load image
 output = remove(input) # remove background
-output.save('BOTTOM_5_B.PNG') # save image
+output.save('save/img/name.png') # save image
