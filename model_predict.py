5f8eb3a211cda9019d7fd592ed4b2f4d84d16139
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-08 15:19:28 +0900

    feat : dataset_add background remove img dir

--- model_predict.py
+++ model_predict.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-test_img_source = '/Users/seunghunjang/Desktop/YOLO_Classification/testset'
+test_img_source = '/Users/seunghunjang/Desktop/YOLO_Classification/background_remove_test'
 
 # Load a model
 model = YOLO('best.pt')
--- yolov8-cls.py
+++ yolov8-cls.py
@@ -3,8 +3,8 @@ from ultralytics import YOLO
 # Load a model
 #model = YOLO('yolov8n-cls.yaml')  # build a new model from YAML
 #model = YOLO('yolov8n-cls.yaml').load('yolov8n-cls.pt')  # build from YAML and transfer weights
-model = YOLO('yolov8m-cls.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8m-cls (2).pt')  # load a pretrained model (recommended for training)
 
 
 # Train the model
-results = model.train(data='/Users/seunghunjang/Desktop/YOLO_Classification/dataset', epochs=30, imgsz=64)
+results = model.train(data='/Users/seunghunjang/Desktop/YOLO_Classification/background_remove_dataset', epochs=30, imgsz=64)
